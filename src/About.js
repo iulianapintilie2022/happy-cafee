8fcbe80bc7cf36e844df1291c6ffc43a2605d345
iulianapintilie2022 <pintilieiuliana2021@gmail.com>
2023-11-17 22:28:17 +0100

    reviews

--- src/About.js
+++ src/About.js
@@ -5,15 +5,33 @@ function About() {
   return (
     <div className="About">
      <div className="responsive-container-block bigContainer">
-  <div className="responsive-container-block Container">
-   
-    <div className="social-icons-container">
-      <a className="social-icon">
-        
-      </a>
+  <div className="container">
+    <div className='centered'> 
+        <div className='review1'>
+          <blockquote>
+            <p>Very quick, great quality, would totally recommend.</p>
+          </blockquote>
+        <h3>Andreea F.</h3>
+        <h4>Christmas LimitedEd</h4>
+    </div>
+        <div className='review1'>
+          <blockquote>
+            <p>These guys know coffee! Good selection, fantastic coffee, super quick delivery.</p>
+          </blockquote>
+        <h3>Matthew</h3>
+        <h4>Florality Coffee</h4>
+    </div>
+    <div className='review1'>
+          <blockquote>
+            <p>Amazing selection of tea, and really delivered to you fresh from the garden! A simply great concept</p>
+          </blockquote>
+        <h3>Kirsten Kaiser</h3>
+        <h4>Herbal LuxuryTea</h4>
+    </div>
+    </div>
     </div>
   </div>
-</div>
+
     </div>
   );
 }
