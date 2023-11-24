9df007a24eb25c442c6d34eb43f639e7be448356
iulianapintilie2022 <pintilieiuliana2021@gmail.com>
2023-11-24 17:37:06 +0100

    christmas

--- src/SaleBanner.js
+++ src/SaleBanner.js
@@ -5,37 +5,37 @@ import bannerImage from './darkheader.png';
 function SaleBanner(){
     return(
 <div className='container'>
-    <div class="card">
-      <div class="card__title">
+    <div className="card">
+      <div className="card__title">
        
       </div>
-      <div class="card__body">
-        <div class="half">
-          <div class="featured_text">
+      <div className="card__body">
+        <div className="half">
+          <div className="featured_text">
             <h1>Christmas</h1>
-            <p class="sub">Specialty Coffee</p>
-            <p class="price">$20</p>
+            <p className="sub">Specialty Coffee</p>
+            <p className="price">$20</p>
           </div>
-          <div class="image">
+          <div className="image">
             <img src={bannerImage} alt="blbla"></img>
           </div>
         </div>
-        <div class="half">
-          <div class="description">
+        <div className="half">
+          <div className="description">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptatem nam pariatur voluptate perferendis, asperiores aspernatur! Porro similique consequatur, nobis soluta minima, quasi laboriosam hic cupiditate perferendis esse numquam magni.</p>
           </div>
-          <span class="stock"><i class="fa fa-pen"></i> In stock</span>
-          <div class="reviews">
+          <span className="stock"><i className="fa fa-pen"></i> In stock</span>
+          <div className="reviews">
             <span>(64 reviews)</span>
           </div>
         </div>
       </div>
-      <div class="card__footer">
-        <div class="recommend">
+      <div className="card__footer">
+        <div className="recommend">
           <p>Texture Type</p>
           <h3 className='texture_description'>Fine Grounded Blend</h3>
         </div>
-        <div class="action">
+        <div className="action">
           <button type="button">See more</button>
         </div>
       </div>
