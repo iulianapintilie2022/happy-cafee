fed4173c3983f2975cdb31868ee62457755aa617
iulianapintilie2022 <pintilieiuliana2021@gmail.com>
2023-11-29 19:51:36 +0100

    sanity

---
+++ sanity-project/schemas/index.js
@@ -0,0 +1,6 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import coffee from './author'
+
+export const schemaTypes = [post, coffee, category, blockContent]
